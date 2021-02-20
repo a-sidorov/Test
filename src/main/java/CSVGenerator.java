2cd9761b8477f3f643460c6830a2284fb6a92a84
BuildTools <unconfigured@null.spigotmc.org>
2021-02-21 03:00:07 +0700

    Initial commit

---
+++ src/main/java/CSVGenerator.java
@@ -0,0 +1,47 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CSVGenerator {
+
+    private static final Map<String, Integer> wordsMap = new HashMap<>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length < 1) {
+            System.err.println("No args");
+            return;
+        }
+
+        //var reader = new Scanner(new File(args[0])).useDelimiter("[^[a-zA-z][0-9]]");
+        var reader = new WordReader(args[0]);
+
+        int count = 0;
+        while (reader.hasNext()) {
+            var word = reader.next();//do smth with word
+
+            if (word.isEmpty()) continue; // пустые строки пропускаем
+            count++;
+
+            if (wordsMap.containsKey(word)) {
+                wordsMap.replace(word, wordsMap.get(word) + 1);
+            } else {
+                wordsMap.put(word, 1);
+            }
+        }
+
+
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(wordsMap.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+
+
+        final int finalCount = count;
+        list.forEach(strInt -> {
+            System.out.println(
+                    String.format("%s,%d,%d", strInt.getKey(), strInt.getValue(), (strInt.getValue() * 100) / finalCount)
+            );
+        });
+    }
+}
---
+++ src/main/java/WordReader.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.Iterator;
+
+public class WordReader implements Iterator<String>, Closeable {
+
+    private final InputStreamReader reader;
+    private boolean hasNext = false;
+
+    WordReader(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+
+
+        reader = new InputStreamReader(new FileInputStream(file));
+        if (file.length() > 0) {
+            hasNext = true;
+        }
+
+    }
+
+    @Override
+    public boolean hasNext() {
+        return hasNext;
+    }
+
+    @Override
+    public String next() {
+        if (!hasNext) {
+            return null;
+        }
+        StringBuilder builder = new StringBuilder();
+        while (true) {
+            try {
+
+                var sym = reader.read();
+                if (sym == -1) {
+                    hasNext = false;
+                    return builder.toString();
+                }
+
+                if (Character.isLetterOrDigit(sym)) {
+                    builder.append((char) sym);
+                } else {
+                    return builder.toString();
+                }
+
+
+            } catch (IOException e) {
+                hasNext = false;
+                return null;
+            }
+
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        reader.close();
+    }
+}
